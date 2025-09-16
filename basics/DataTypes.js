5662100b0c1cd907c507663c1ad79457e2bacf48
Usman Rasheed <usmanrasheed.dev@gmail.com>
2025-09-16 19:59:20 +0000

    Add DataTypes.js: JavaScript data types examples

---
+++ basics/DataTypes.js
@@ -0,0 +1,19 @@
+// String
+let city = "Lahore";
+console.log(city);
+
+// Number
+let score = 95;
+console.log(score);
+
+// Boolean
+let isPassed = true;
+console.log(isPassed);
+
+// Null
+let futureValue = null;
+console.log(futureValue);
+
+// Undefined
+let data;
+console.log(data);
