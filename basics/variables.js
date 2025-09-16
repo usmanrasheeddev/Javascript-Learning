3b006bc0d446623a991d676a8901350b0892fa52
Usman Rasheed <usmanrasheed.dev@gmail.com>
2025-09-16 19:55:59 +0000

    Add variables.js: variable example with userName

---
+++ basics/variables.js
@@ -0,0 +1,5 @@
+let userName="Usman_Rasheed";
+console.log(userName);      //Declared a variable and initialized
+
+userName2="Usman";          //assigned a new one
+console.log(userName2);      //Logged the variable again
